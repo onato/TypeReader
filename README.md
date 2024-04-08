f48e2ee2c70c642f843fc2a0ee9ec19bcfe814fa
Stephen Williams <107999+onato@users.noreply.github.com>
2024-04-08 14:00:57 +1200

    Initial commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TypeReader
+An app that can open a pdf and read it to you from your pocket whilst you are going for a walk
---
+++ TypeReader/TypeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeReaderApp.swift
+//  TypeReader
+//
+//  Created by Stephen Williams on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TypeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
